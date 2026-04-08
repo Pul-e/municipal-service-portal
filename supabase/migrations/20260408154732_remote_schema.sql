cdb4d74da437ba107164de5fbfc6ef394cfdf448
2832207 <2832207@students.wits.ac.za>
2026-04-08 17:53:57 +0200

    Add initial database migration

---
+++ supabase/migrations/20260408154732_remote_schema.sql
@@ -0,0 +1,98 @@
+drop extension if exists "pg_net";
+
+create extension if not exists "postgis" with schema "public";
+
+set check_function_bodies = off;
+
+create type "public"."geometry_dump" as ("path" integer[], "geom" public.geometry);
+
+CREATE OR REPLACE FUNCTION public.rls_auto_enable()
+ RETURNS event_trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO 'pg_catalog'
+AS $function$
+DECLARE
+  cmd record;
+BEGIN
+  FOR cmd IN
+    SELECT *
+    FROM pg_event_trigger_ddl_commands()
+    WHERE command_tag IN ('CREATE TABLE', 'CREATE TABLE AS', 'SELECT INTO')
+      AND object_type IN ('table','partitioned table')
+  LOOP
+     IF cmd.schema_name IS NOT NULL AND cmd.schema_name IN ('public') AND cmd.schema_name NOT IN ('pg_catalog','information_schema') AND cmd.schema_name NOT LIKE 'pg_toast%' AND cmd.schema_name NOT LIKE 'pg_temp%' THEN
+      BEGIN
+        EXECUTE format('alter table if exists %s enable row level security', cmd.object_identity);
+        RAISE LOG 'rls_auto_enable: enabled RLS on %', cmd.object_identity;
+      EXCEPTION
+        WHEN OTHERS THEN
+          RAISE LOG 'rls_auto_enable: failed to enable RLS on %', cmd.object_identity;
+      END;
+     ELSE
+        RAISE LOG 'rls_auto_enable: skip % (either system schema or not in enforced list: %.)', cmd.object_identity, cmd.schema_name;
+     END IF;
+  END LOOP;
+END;
+$function$
+;
+
+create type "public"."valid_detail" as ("valid" boolean, "reason" character varying, "location" public.geometry);
+
+grant delete on table "public"."spatial_ref_sys" to "anon";
+
+grant insert on table "public"."spatial_ref_sys" to "anon";
+
+grant references on table "public"."spatial_ref_sys" to "anon";
+
+grant select on table "public"."spatial_ref_sys" to "anon";
+
+grant trigger on table "public"."spatial_ref_sys" to "anon";
+
+grant truncate on table "public"."spatial_ref_sys" to "anon";
+
+grant update on table "public"."spatial_ref_sys" to "anon";
+
+grant delete on table "public"."spatial_ref_sys" to "authenticated";
+
+grant insert on table "public"."spatial_ref_sys" to "authenticated";
+
+grant references on table "public"."spatial_ref_sys" to "authenticated";
+
+grant select on table "public"."spatial_ref_sys" to "authenticated";
+
+grant trigger on table "public"."spatial_ref_sys" to "authenticated";
+
+grant truncate on table "public"."spatial_ref_sys" to "authenticated";
+
+grant update on table "public"."spatial_ref_sys" to "authenticated";
+
+grant delete on table "public"."spatial_ref_sys" to "postgres";
+
+grant insert on table "public"."spatial_ref_sys" to "postgres";
+
+grant references on table "public"."spatial_ref_sys" to "postgres";
+
+grant select on table "public"."spatial_ref_sys" to "postgres";
+
+grant trigger on table "public"."spatial_ref_sys" to "postgres";
+
+grant truncate on table "public"."spatial_ref_sys" to "postgres";
+
+grant update on table "public"."spatial_ref_sys" to "postgres";
+
+grant delete on table "public"."spatial_ref_sys" to "service_role";
+
+grant insert on table "public"."spatial_ref_sys" to "service_role";
+
+grant references on table "public"."spatial_ref_sys" to "service_role";
+
+grant select on table "public"."spatial_ref_sys" to "service_role";
+
+grant trigger on table "public"."spatial_ref_sys" to "service_role";
+
+grant truncate on table "public"."spatial_ref_sys" to "service_role";
+
+grant update on table "public"."spatial_ref_sys" to "service_role";
+
+
